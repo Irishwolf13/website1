74edc4df74c41c3fb564199e1bd9b24c4c21861f
John Rooney <rooneyjohn@gmail.com>
2022-12-26 22:42:36 -0500

    Playing around with CSS

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-let currrentWord = 'boring'
-const remoteUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+document.addEventListener('DOMContentLoaded', async () => {
+    
+    let currrentWord = 'Cold'
+    const remoteUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
-// Gather up the Troops
-const listContainer = document.querySelector('.synonim-List')
+    // Gather up the Troops
+    const listContainer = document.querySelector('.synonym-List')
+    const titleBar = document.querySelector('.titlebar')
 
-function getRemoteWord(myWord) {
-    fetch(`${remoteUrl}${myWord}`, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-    })
-    .then(response => response.json()) 
-    .then(myWord => {
-        //console.log(myWord)
-        myWord.forEach(element => {
-            element.meanings.forEach(element2 => {
-                if (element2.synonyms.length > 0) {
-                    element2.synonyms.forEach(myItem => {
-                        let li = document.createElement('li')
-                        li.innerHTML = myItem
-                        listContainer.appendChild(li)
-                    })
-                    //console.log(element2.synonyms)
-                }
-            })
+    function getRemoteWord(myWord) {
+        fetch(`${remoteUrl}${myWord}`, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
         })
-    })
-};
+        .then(response => response.json()) 
+        .then(myWord => {
+            //console.log(myWord)
+            myWord.forEach(element => {
+                element.meanings.forEach(element2 => {
+                    if (element2.synonyms.length > 0) {
+                        element2.synonyms.forEach(myItem => {
+                            let li = document.createElement('li')
+                            li.innerHTML = myItem
+                            li.classList.add('list-Item')
+                            listContainer.appendChild(li)
+                        })
+                    }
+                })
+            })
+            
+            // This should eventually get the word from the dictionary and make it the current word
+            let newWord = currrentWord
+            adjustMainWord(newWord)
+        })
+    };
 
-function getWordList() {
-    const fs = require('fs')
-    const rawData = fs.readFileSync("english.txt", 'utf-8')
-    let dataArray = rawData.split("\n");  
-    currrentWord = dataArray[13285]
-}
-
-//getWordList();
-getRemoteWord(currrentWord);
+    function adjustMainWord() {
+        titleBar.innerHTML = currrentWord
+    }
+    //getWordList();
+    getRemoteWord(currrentWord);
+})
