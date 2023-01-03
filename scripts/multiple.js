c579aa58b6c7275752bf69f77fc804247c9735c4
Matthew <turbodieseldodge@gmail.com>
2023-01-03 13:27:31 -0600

    working on def game

--- scripts/multiple.js
+++ scripts/multiple.js
@@ -4,18 +4,27 @@ function getWordMultipleChoice(myWord) {
     setPoints(numberOfPoints)
     listContainer.classList.remove('synonym-List')
     listContainer.classList.add('synonym-List-Multiple')
-    
-    let selectionArray = Object.keys(_APIsynonyms)
-    let selectionRandomNumber = Math.floor(Math.random() * selectionArray.length)
-    let mySelection = selectionArray[selectionRandomNumber]
-    let button = document.createElement('button')
-    button.id = `${mySelection}`
-    button.classList.add('multipleChoiceButton')
-    button.innerHTML = (mySelection).toLocaleUpperCase()
-    listContainer.appendChild(button)
-    
-    adjustMainWord(currentWord, 'Multiple Choice')
-    
     // Sets up next word
     setUpNewWord()
+
+    let newArray = ['myWord', 'myWord', 'myWord', 'myWord']
+    let myNumber = Math.floor(Math.random() * newArray.length)
+    console.log(myNumber)
+    newArray[myNumber] = myWord
+
+
+
+
+    // console.log(_currentAPIdefinitions)
+    adjustMainWord(`${_currentAPIdefinitions[0]}`, 'Multiple')
+// create button w/label
+        for (let i = 0; i < 4; i++) {
+        let button = document.createElement('button')
+        button.innerText = newArray[i]
+        button.addEventListener('click', (e) => {
+            console.log('clicked')
+        })
+        listContainer.appendChild(button)
+    }
+
 }
